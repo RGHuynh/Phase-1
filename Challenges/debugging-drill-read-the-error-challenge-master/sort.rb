e837a54e33a0ade3f82d60afefde620b8a08a820
RGHuynh <randall.g.huynh@gmail.com>
2017-12-05 10:46:48 -0500

    Add new method to pass sorts numbers and strings
    Wrote an is_integer? method to return an Integer value that would be
    used in another method ordering_objects. In the ordering_objects, the
    parameter takes an array as an argument and reorder each numbers and
    return a sorted array.

--- Challenges/debugging-drill-read-the-error-challenge-master/sort.rb
+++ Challenges/debugging-drill-read-the-error-challenge-master/sort.rb
@@ -1,19 +1,17 @@
 # Rewrite this method with your own sorting algorithm
 # Make your algorithm pass each of the tests in spec/sort_spec.rb
 
-def is_interger?(n)
+def is_integer?(n)
   if n.class != Integer
     return n.to_i
   end
   n
 end
 
-
 def sort_nil(objects)
   nil_count = objects.count(nil)
   objects.delete(nil)
   object_without_nil = objects.sort
-
   until nil_count == 0
     object_with_nil = object_without_nil.insert(0,nil)
     nil_count -= 1
@@ -21,30 +19,31 @@ def sort_nil(objects)
   return object_with_nil
 end
 
-def ordering_objects(o)
 
-  object_count = o.length - 1
-  counter = 0
-
-  until start == object_count
-    if is_interger(o[start]) > is_interger(o[start + 1])
-      o.insert(start, o.delete_at(start + 1))
-    end
-    start += 1
-  end 
+def result(object)
+  results = []
+  object.each do |x|
+    bl = x.is_a? String
+    results << bl
+  end
+  results.uniq.length == 1
 end
 
-def sort(objects)
-  
-  if objects.include?(nil) == true
-    sort_nil(objects)
-  else
-    objects.sort
+
+def ordering_objects(o)
+  o.sort do |x,y|
+    is_integer?(x) <=> is_integer?(y)
   end
 end
 
-# identify the integers from strings
-# temporaly convert string to integer
-# if the number before the integer is smaller move it to first place
-#   do it till no more number is left
-# i want to move the integer or the string into the correct order
+
+def sort(objects)
+  if objects.include?(nil) == true
+    sort_nil(objects)
+  elsif result(objects) == true
+    objects.sort
+  else
+    ordering_objects(objects)
+  end
+end
+
