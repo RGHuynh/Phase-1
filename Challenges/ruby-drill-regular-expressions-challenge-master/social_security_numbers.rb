e0b7324e007a134cd73ea16e4bd96502f6d50d8d
RGHuynh <randall.g.huynh@gmail.com>
2017-12-06 16:18:48 -0500

    Add return for any SSN it finds

--- Challenges/ruby-drill-regular-expressions-challenge-master/social_security_numbers.rb
+++ Challenges/ruby-drill-regular-expressions-challenge-master/social_security_numbers.rb
@@ -23,9 +23,19 @@ end
 
 # Obfuscate all Social Security Numbers. Example: XXX-XX-4430.
 def hide_all_ssns(string)
+  string.gsub(/\d{3}\D\d{2}/, 'XXX-XX')
 end
 
 # Format all Social Security Numbers to use single dashes for delimiters:
 # '480014430', '480.01.4430', and '480--01--4430' would all be formatted '480-01-4430'.
 def format_ssns(string)
+    correct_ssn = []
+    store = ""
+    ssn = string.scan(/\d+.\d{2}.\d{4}|\d{3}--\d{2}--\d{4}/)
+    ssn.each {|n| correct_ssn << n.gsub(/(\d{3})[^\d]?[\--]?(\d{2})[^\d]?[\--]?(\d{4})/, '\1-\2-\3')}
+    p store1 = string.scan(/[a-z, A-Z]+/)
+    store3 = store1 + correct_ssn
+    store3.each {|x| store << "#{x} "}
+    store
+
 end
