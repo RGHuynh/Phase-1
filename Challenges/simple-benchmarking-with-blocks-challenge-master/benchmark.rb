9be3a43c35fda728600f4a153d4190e62dd82ab8
RGHuynh <randall.g.huynh@gmail.com>
2017-12-06 19:46:21 -0500

    Add method to cal how long it takes to run code

--- Challenges/simple-benchmarking-with-blocks-challenge-master/benchmark.rb
+++ Challenges/simple-benchmarking-with-blocks-challenge-master/benchmark.rb
@@ -1,4 +1,10 @@
 # Accepts a block and returns how many seconds
 # it takes to execute the code in the block.
 def benchmark
+  reverse_start_time = Time.now
+  yield
+  reverse_end_time = Time.now
+  reverse_run_time = reverse_end_time - reverse_start_time
 end
+
+
