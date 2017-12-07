98ec9a2563d8c88c58f447066a0235f8d964e684
RGHuynh <randall.g.huynh@gmail.com>
2017-12-07 10:21:47 -0500

    Add a method for factorial iterative
    Created a number variable equal to 1 so that it can start counting
    upward from 1 to the amount of number the input is. The if statement is
    used to return the 1 if there is a 0 input. The for loop is used
    to keep track of the amount of iteration it goes through.

--- Challenges/algorithm-drill-factorial-challenge-master/factorial.rb
+++ Challenges/algorithm-drill-factorial-challenge-master/factorial.rb
@@ -1,4 +1,14 @@
 def factorial_iterative(n)
+    number = 1
+    if n == 0
+      return 0
+    end
+    
+    for i in 1..n
+       p number = i * number
+    end
+
+    return number
 end
 
 def factorial_recursive(n)
