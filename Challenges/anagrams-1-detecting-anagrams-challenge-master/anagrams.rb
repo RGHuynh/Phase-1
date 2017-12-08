70d2ba0857fcba34f47ee99ca63bc25dad681db7
RGHuynh <randall.g.huynh@gmail.com>
2017-12-07 19:15:19 -0500

    Refactor anagram method to make it more effecient

--- Challenges/anagrams-1-detecting-anagrams-challenge-master/anagrams.rb
+++ Challenges/anagrams-1-detecting-anagrams-challenge-master/anagrams.rb
@@ -1,1 +1,24 @@
+def anagrams?(word, word2)
+    anagram_canonical_form(word) == anagram_canonical_form(word2)
+    # word1 = word.split('')
+    # word2 = word2.split('')
+    # result = []
 
+    # if word1.length == word2.length
+    #     word1.each do |w|
+    #     result << word2 .include?(w)
+    # end
+    # else 
+    #     return false
+    # end
+
+    # if result.uniq.length == 1
+    #     return true
+    # else
+    #     false
+    # end
+end
+
+def anagram_canonical_form(word)
+    word.upcase.split('').sort.join
+end
--- Challenges/anagrams-1-detecting-anagrams-challenge-master/spec/anagrams_spec.rb
+++ Challenges/anagrams-1-detecting-anagrams-challenge-master/spec/anagrams_spec.rb
@@ -1,4 +1,14 @@
 require_relative '../anagrams'
 
 describe 'anagrams?' do
+    it 'to check if two words are anigram' do
+        expect(anagrams?('ab','ba')).to eq true
+    end
+
+    it 'to check if edge case works in anigram' do
+        expect(anagrams?('MELON', 'lemon')).to eq true
+    end
+    it 'to check if non eglish works in anigram' do
+        expect(anagrams?('abcde2', 'c2abed')).to eq true
+    end
 end
