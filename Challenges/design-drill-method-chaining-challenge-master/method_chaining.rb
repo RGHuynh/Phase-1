88e9c06dca82d2dcbec77c7e47302cff5e4effbf
RGHuynh <randall.g.huynh@gmail.com>
2017-12-10 20:54:09 -0500

    Refactor squared_primes method

--- Challenges/design-drill-method-chaining-challenge-master/method_chaining.rb
+++ Challenges/design-drill-method-chaining-challenge-master/method_chaining.rb
@@ -3,9 +3,13 @@ def shout_backwards(string)
   all_caps = string.upcase.reverse + "!!!"
 end
 
-This is convoluted. Refactor for clarity.
+# This is convoluted. Refactor for clarity.
 def squared_primes(array)
-  array.find_all{|x| (2..x-1).select(){|i| x % i == 0}.count == 0 && x > 1}.map{|p| p*p}
+  array.find_all do |x| 
+    (2..x-1).select() do |i| 
+      x % i == 0 
+    end.count == 0 && x > 1 
+  end.map{|p| p*p}
 end
 
 
--- Challenges/design-drill-method-chaining-challenge-master/spec/method_chaining_spec.rb
+++ Challenges/design-drill-method-chaining-challenge-master/spec/method_chaining_spec.rb
@@ -10,7 +10,6 @@ describe "squared_primes" do
   it 'returns an array of the prime numbers, squared' do
     actual_result = squared_primes([1, 3, 4, 7, 42])
     expected_result = [9, 49]
-
     expect(actual_result).to match_array(expected_result)
   end
 end
