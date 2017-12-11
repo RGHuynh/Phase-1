0aa7943c022bfc39d9617fe4abc1e19a6b682f93
RGHuynh <randall.g.huynh@gmail.com>
2017-12-11 16:47:44 -0500

    Refractor hyphenate with reduce method

--- Challenges/ruby-drill-enumerable-methods-challenge-master/aggregate.rb
+++ Challenges/ruby-drill-enumerable-methods-challenge-master/aggregate.rb
@@ -3,19 +3,17 @@ def sum(numbers, starting_value = 0)
   numbers.reduce(starting_value) do |value, number|
     value += number
   end
-  
+
 end
 
 def hyphenate(words)
-  hyphenated_words = ""
 
-  words.each do |word|
-    if hyphenated_words.empty?
-      hyphenated_words += word
+  words.reduce(String.new) do |string, word|
+    if string.empty?
+      string += "#{word}"
     else
-      hyphenated_words += "-#{word}"
+      string += "-#{word}"
     end
   end
 
-  hyphenated_words
 end
