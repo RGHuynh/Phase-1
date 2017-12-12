2ac17a1c17f5bc577e0728e06f5dd0ec39abb2e1
RGHuynh <randall.g.huynh@gmail.com>
2017-12-12 12:06:30 -0500

    Add shuffle method to file
    The shuffle method takes in an array and does two things, 1) it checks
    if it has any symbols, if so, it will return an unshuffl array. 2)
    if it passes the first condition, the second part of the body will loop
    through the amount of element in the array and swap the last index of the
    element with a random element generated from 0 to one less the last element.

--- Challenges/algorithm-drill-shuffle-challenge-master/shuffle.rb
+++ Challenges/algorithm-drill-shuffle-challenge-master/shuffle.rb
@@ -1,2 +1,16 @@
 def shuffle(array)
+  random = Random.new
+  last_number = (array.count - 1)
+  random_last_number = last_number - 1
+  
+  if array.find {|x| x.class == Symbol}
+    return array
+  end
+  
+  for i in 1..(array.count-1)
+    array[last_number], array[random.rand(random_last_number)] = array[random.rand(random_last_number)], array[last_number]
+    last_number -= 1
+  end
+  
+  array
 end
